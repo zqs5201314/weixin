76e731ca4e264c70e5937d4c652d52795cd5b632
zqs5201314 <111162361+zqs5201314@users.noreply.github.com>
2022-08-13 11:05:17 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,20 +1,20 @@
 # 公众号配置
 # 公众号appId
-app_id = ""
+app_id = "wxa0881410cee2fce0"
 # 公众号appSecret
-app_secret = ""
+app_secret = "49c24eea1c4644b3c446ce13372dd3a3"
 # 模板消息id
-template_id = ""
+template_id = "	mF007Tvg9mG--iXWPG-FaAY-nsEqia08JoZ02F0XqYg"
 # 接收公众号消息的微信号
-user = ""
+user = "oLXNv540_cG_ObR6aoGwwrdNW-Ws"
 
 # 信息配置
 # 所在省份
-province = "广东"
+province = "海南"
 # 所在城市
-city = "广州"
+city = "陵水"
 # 生日，如果月份或者日期小于10，直接用对应的数字即可，例如1997-1-1
-birthday = "1997-1-1"
+birthday = "2022-1-1"
 # 在一起的日子，格式同上
-love_date = "2022-1-1"
+love_date = "2022-8-5"
 
